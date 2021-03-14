a68fc866d2dbbded3b8f1f145c060cf7baaaec94
Ken <18917927962@163.com>
2021-03-14 23:22:27 +0800

    week06 homework

---
+++ Week_06/最小路径和.java
@@ -0,0 +1,23 @@
+package com.ken.动态规划;
+
+public class 最小路径和 {
+
+    public int minPathSum(int[][] grid) {
+        int m = grid[0].length;
+        int n = grid.length;
+        int[][] dp = new int[n][m];
+        dp[0][0] = grid[0][0];
+        for (int i = 1; i < n; i++) {
+            dp[i][0] = dp[i - 1][0] + grid[i][0];
+        }
+        for (int j = 1; j < m; j++) {
+            dp[0][j] = dp[0][j - 1] + grid[0][j];
+        }
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < m; j++) {
+                dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
+            }
+        }
+        return dp[n - 1][m - 1];
+    }
+}
